12d3eb33ddb3248a5d9eee4f3760d8a03605f9c4
Andres0203 <fandressanchezm@gmail.com>
2022-11-27 20:25:02 -0500

    Erase data

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:thecat_hackerrank/models/catclasses.dart';
 
 void main() => runApp(const CatClass());
 
